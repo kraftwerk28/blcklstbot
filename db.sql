ff990d8649805827de32a346a2639bc9abf41aed
kraftwerk28 <kefirchik3@gmail.com>
2020-03-22 03:24:11 +0200

    chore: prune files

--- db.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE groups (
-  group_id BIGINT PRIMARY KEY,
-  group_username VARCHAR,
-  silent BOOLEAN NOT NULL DEFAULT TRUE,
-  legal BOOLEAN NOT NUll DEFAULT FALSE -- if admins can add to blacklists
-);
