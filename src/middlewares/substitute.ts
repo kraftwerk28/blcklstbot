25ad3da043b95a8e76012eb84e746c2fadb105a4
kraftwerk28 <kefirchik3@gmail.com>
2021-05-13 15:25:14 +0300

    Allow multiple substitutions

--- src/middlewares/substitute.ts
+++ src/middlewares/substitute.ts
@@ -1,21 +1,23 @@
 import { log } from '../logger';
 import { OnMiddleware } from '../types';
 
+const substRe = /s\/((?:\\\/|[^\/])+)\/((?:\\\/|[^\/])*)(?:\/([mig]*))?(?:$|\s+)/g;
+
 export const substitute: OnMiddleware<'text'> = async function (ctx, next) {
   const reply = ctx.message.reply_to_message;
   if (!reply || !('text' in reply)) return next();
-  const match = ctx.message.text.match(
-    /^s\/((?:\\\/|[^\/])+)\/((?:\\\/|[^\/])*)(?:\/([igm]*))?$/,
-  );
-  if (!match) return next();
-  let [, from, to, flags] = match;
-  to = to.replace(/\\([&\d])/g, '$$$1').replace(/\$0/g, '$$&');
-  try {
-    const fromRe = new RegExp(from, flags);
-    // Convert \1 -> $1, \2 -> $2 etc
-    log.info('Regex substitution: %O', { fromRe, to });
-    const replyOptions = { reply_to_message_id: reply.message_id };
-    const finalText = reply.text.replace(fromRe, to);
-    await ctx.reply(finalText, replyOptions);
-  } catch {}
+  let finalText = reply.text;
+  for (const match of ctx.message.text.matchAll(substRe)) {
+    const replaceTo = match[2]
+      .replace(/\\([&\d])/g, '$$$1')
+      .replace(/\$0/g, '$$&');
+    try {
+      // Catch bad regex syntax
+      const replaceFrom = new RegExp(match[1], match[3]);
+      finalText = finalText.replace(replaceFrom, replaceTo);
+      log.info('Regex substitution: %O', { replaceFrom, replaceTo, finalText });
+    } catch {}
+  }
+  const replyOptions = { reply_to_message_id: reply.message_id };
+  return ctx.reply(finalText, replyOptions);
 };
