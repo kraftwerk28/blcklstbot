ef1e0c4eda63950d1a0c4266739b196c46e2a2c4
kraftwerk28 <kefirchik3@gmail.com>
2021-05-16 18:09:27 +0300

    Fix so that command can be undef-ed only by creator or admin

--- src/commands/bang-handler.ts
+++ src/commands/bang-handler.ts
@@ -1,19 +1,15 @@
-import { Composer } from '../composer';
-import { isGroupChat, senderIsAdmin } from '../guards';
 import { HearsMiddleware } from '../types';
+import { noop } from '../utils';
 
-export const bangHandler = Composer.guardAll(
-  [isGroupChat, senderIsAdmin],
-  async function (ctx) {
-    const reply = ctx.message.reply_to_message;
-    const command = await ctx.dbStore.getCommand(ctx.match[1], ctx.chat.id);
-    if (command) {
-      await ctx.tg.copyMessage(
-        ctx.chat.id,
-        +process.env.COMMANDS_CHANNEL_ID!,
-        command.message_id,
-        { reply_to_message_id: reply?.message_id ?? ctx.message.message_id },
-      );
-    }
-  } as HearsMiddleware,
-);
+export const bangHandler = async function (ctx, next) {
+  const { chat, match, message, tg } = ctx;
+  const reply = message.reply_to_message;
+  const channelId = +process.env.COMMANDS_CHANNEL_ID!;
+  const dbCommand = await ctx.dbStore.getCommand(match[1], chat.id);
+  if (!dbCommand) return next();
+  await tg
+    .copyMessage(chat.id, channelId, dbCommand.message_id, {
+      reply_to_message_id: reply?.message_id ?? message.message_id,
+    })
+    .catch(noop);
+} as HearsMiddleware;
